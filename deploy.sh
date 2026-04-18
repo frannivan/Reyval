a378f62e432b10d20219f7f74bcca8015a818cd2
frannivan <franciscoivansaldivar@gmail.com>
2026-04-18 22:42:14 +0900

    fix: ajustar apiUrl y añadir seed de base de datos

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@ echo "📦 Configurando el Backend..."
 cd backend-api
 npm install
 npx prisma generate
+npx prisma db push --accept-data-loss
+node prisma/seed.js
 npm run build
 
 if [ $? -eq 0 ]; then
-    echo "✅ Backend (NestJS) listo."
+    echo "✅ Backend (NestJS) listo y con datos."
 else
     echo "❌ Error al construir el backend. Abortando."
     exit 1
